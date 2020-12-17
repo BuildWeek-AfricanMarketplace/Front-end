e7e97539004f3fb01fa638148eaddf11260226a5
Nicholas Watson <ngwatso@Nicholass-iMac.hsd1.mi.comcast.net>
2020-12-16 21:40:40 -0500

    set up basic components

---
+++ buildweek-app/src/components/Reg.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function Register() {
+	const [userName, setUserName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+}
